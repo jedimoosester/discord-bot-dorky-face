6287ac2dea9e5aa70082c592790e14534cd7f131
jedimoosester <kyhf.misc@gmail.com>
2024-07-10 21:25:31 -0700

    remove ()

--- lib/bot/__init__.py
+++ lib/bot/__init__.py
@@ -93,7 +93,7 @@ class Bot(BotBase):
         await self.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="you... 🎃"))
 
     async def test_response(self):
-        await self.application_info().name
+        await self.application_info()
 
     async def on_message(self, message):
         if not message.author.bot:
